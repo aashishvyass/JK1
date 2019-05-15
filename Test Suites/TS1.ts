9f900af100a46f8a925f3ef61cb539e1331cb925
Ashish <aashishvyass@gmail.com>
2019-05-16 01:25:42 +0530

    second commit

---
+++ Test Suites/TS1.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>838e662f-dfad-4b4b-8662-9a5d46247c7c</testSuiteGuid>
+   <testCaseLink>
+      <guid>cc748851-4295-4c55-a094-4ebc4ed4df5f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/T1</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>eda7b0b9-8603-4e2d-b367-36610c57fd0a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/T2</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
